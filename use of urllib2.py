18cf7952d2b1af7975d8fa1ba81cf411e15e224a
jiean yang <yja8534786@gmail.com>
2017-12-07 17:59:15 +0100

    first web scraping

---
+++ use of urllib2.py
@@ -0,0 +1,16 @@
+import urllib.request as urllib2
+
+def download(url, num_retries=2):
+	print ('Downloading:',url)
+	try:
+		html = urllib2.urlopen(url).read()
+	except urllib2.URLError as e:
+		print ('Download error:', e.reason)
+		html = None
+		if num_retries > 0:
+			if hasattr(e, 'code') and 500 <= e.code < 600:
+				#recursively retry 5xx HTTP errors
+				return download(url, num_retries-1)
+	return html
+	
+print(download('http://example.webscraping.com/'))
