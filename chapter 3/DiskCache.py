dac67c6822fb6907d75aca04932f663731104dca
Quentinnnyang <yja8534786@gmail.com>
2018-01-11 16:46:41 +0100

    缓存过期更新功能

--- chapter 3/DiskCache.py
+++ chapter 3/DiskCache.py
@@ -4,9 +4,12 @@ from urllib import parse as urlparse
 import pickle
 import zlib
 
+from datetime import datetime, timedelta
+
 class DiskCache:
-	def __init__(self, cache_dir='cache'):
+	def __init__(self, cache_dir='cache', expires=timedelta(days=30)):
 		self.cache_dir = cache_dir
+		self.expires = expires
 		# self.max_length = max_length
 		
 	def url_to_path(self, url):
@@ -38,10 +41,14 @@ class DiskCache:
 		path = self.url_to_path(url)
 		if os.path.exists(path+'.pkl'):
 			with open(path+'.pkl', 'rb') as fp:
+				result, timestamp = pickle.loads(zlib.decompress(fp.read()))
+				if self.has_expired(timestamp):
+					raise AttributeError(url + ' has expired')
+				return result
 				# 反序列化，恢复文件原始数据类型
 				## return pickle.load(fp)
 				# 解压缩
-				return pickle.loads(zlib.decompress(fp.read()))
+				# return pickle.loads(zlib.decompress(fp.read()))
 		else:
 			#URL has not yet been cached
 			raise AttributeError(url + ' does not exist')
@@ -49,13 +56,22 @@ class DiskCache:
 	def __setitem__(self, url, result):
 		"""Save data to disk for this url
 		"""
+
 		path = self.url_to_path(url)
 		# 用于去掉文件名，返回目录所在的路径
 		folder = os.path.dirname(path)
 		if not os.path.exists(folder):
 			os.makedirs(folder)
+		
+		timestamp = datetime.now()
+		data = pickle.dumps((result,timestamp))
 		with open(path+'.pkl', 'wb') as fp:
-			#pickle 会将输入转换成序列化字符串然后保存在磁盘中
+			# pickle 会将输入转换成序列化字符串然后保存在磁盘中
 			## pickle.dump(result, fp)
 			# 使用压缩 节省磁盘空间
-			fp.write(zlib.compress(pickle.dumps(result)))
+			fp.write(zlib.compress(data))
+
+	def has_expired(self, timestamp):
+		""" Return whether this timestamp has expired
+		"""
+		return datetime.now() > timestamp + self.expires
--- chapter 3/test.py
+++ chapter 3/test.py
@@ -2,6 +2,8 @@ import re
 import pickle
 import os
 import zlib
+from datetime import datetime, timedelta
+
 url = 'http://example.webscraping.com/places/default/view/Antarctica-9'
 a = re.sub('[^/0-9a-zA-Z\-.,;_]', '_', url)
 c = a+'/'
@@ -24,3 +26,25 @@ if os.path.exists(path):
 		# fp.write(zlib.compress(pickle.dumps('result')))
 		# 读取文件，模式为 rb
 		print(pickle.loads(zlib.decompress(fp.read())))
+
+def has_expired(timestamp):
+		""" Return whether this timestamp has expired
+		"""
+		return datetime.now() > timestamp + timedelta(seconds=5)
+
+path = r'C:\Users\yang\Desktop\Web Scraping with Python\chapter 3\cache\example.webscraping.com\places\default\index\1'
+if os.path.exists(path+'.pkl'):
+	with open(path+'.pkl', 'rb') as fp:
+		result, timestamp = pickle.loads(zlib.decompress(fp.read()))
+		if has_expired(timestamp):
+			raise AttributeError(url + 'has expired')
+		print(result)
+		# 反序列化，恢复文件原始数据类型
+		## return pickle.load(fp)
+		# 解压缩
+		# return pickle.loads(zlib.decompress(fp.read()))
+else:
+	#URL has not yet been cached
+	raise AttributeError(url + ' does not exist')
+
+
