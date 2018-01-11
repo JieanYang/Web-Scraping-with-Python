6a639cd9fe4b8cbcd089e72f0b6ec63e89e5f6b3
Quentinnnyang <yja8534786@gmail.com>
2018-01-11 14:24:07 +0100

    完全支持缓存功能

--- chapter 3/test.py
+++ chapter 3/test.py
@@ -1,4 +1,6 @@
 import re
+import pickle
+import os
 url = 'http://example.webscraping.com/places/default/view/Antarctica-9'
 a = re.sub('[^/0-9a-zA-Z\-.,;_]', '_', url)
 c = a+'/'
@@ -13,3 +15,8 @@ print(filename)
 
 print(c.split('/'))
 print('/'.join(segment[:3] for segment in c.split('/')))
+
+path = r'C:\Users\yang\Desktop\Web Scraping with Python\chapter 3\cache\example.webscraping.com\places\default\index\8'
+if os.path.exists(path+'.pkl'):
+	with open(path+'.pkl', 'rb') as fp:
+		print (pickle.load(fp))
